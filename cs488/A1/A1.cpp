a82e955b20ae86d3a5f7784cc626e8811b7f874c
Clara Kang <heng.kang@mail.mcgill.ca>
2018-09-25 12:27:09 -0400

    fix mouse button

--- cs488/A1/A1.cpp
+++ cs488/A1/A1.cpp
@@ -49,7 +49,8 @@ A1::A1()
 		zoom_factor( 1 ),
 		persistent_rotation_dir( 0 ),
 		maze_created( false ),
-		shift_down( false )
+		shift_down( false ),
+		m_mouseButtonActive( false );
 {
 	colour[0] = floor_col.x;
 	colour[1] = floor_col.y;
